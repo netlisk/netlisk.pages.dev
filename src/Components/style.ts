875a4805e10ecf9807a556d685af0ca795963d78
Eric Chang <changeric77@gmail.com>
2022-09-13 01:43:12 +1000

    Cleanup.

--- src/Components/style.ts
+++ src/Components/style.ts
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 
-const MapOverlayVisible = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-
-    width: 20%;
-    height: 50%;
-    position: fixed;
-    bottom: 5px;
-    right: 5px;
-    background-color: rgba(255,255,255,0.7);
-    
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-    line-height: 18px;
-    
-`;
+// const MapOverlayVisible = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+//     align-items: flex-start;
+//
+//     width: 20%;
+//     height: 50%;
+//     position: fixed;
+//     bottom: 5px;
+//     right: 5px;
+//     background-color: rgba(255,255,255,0.7);
+//
+//     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+//     line-height: 18px;
+//
+// `;
 
 const FloatingDiv = styled.div`
     width: 30vh;
@@ -44,4 +44,4 @@ const MapOverlayNavbarIcon = styled.img`
     width: 5vh;
 `;
 
-export {FloatingToggle, MapOverlayVisible, MapOverlayNavbar, MapOverlayNavbarIcon, FloatingDiv};
+export {FloatingToggle, MapOverlayNavbar, MapOverlayNavbarIcon, FloatingDiv};
